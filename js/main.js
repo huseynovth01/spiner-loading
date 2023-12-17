cf8ce28b6bb653c7676c2928e477411af1e84c4c
mac book pro <macbookpro@macs-MacBook-Pro.local>
2023-12-17 18:34:45 +0400

    restart

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-var loader = document.querySelector(".loader");
-var overlay = document.querySelector(".overlay");
+// var loader = document.querySelector(".loader");
+// var overlay = document.querySelector(".overlay");
 
-setTimeout(function(){
-    loader.style.display = 'none';
-    overlay.style.display = 'none';
-},5000)
+// setTimeout(function(){
+//     loader.style.display = 'none';
+//     overlay.style.display = 'none';
+// },5000)
 
 
 
