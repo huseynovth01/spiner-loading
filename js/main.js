6e4889210631b296159a1844e9eebc63e1079b29
mac book pro <macbookpro@macs-MacBook-Pro.local>
2023-12-17 18:20:13 +0400

    spiner is done

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var loader = document.querySelector(".loader");
+var overlay = document.querySelector(".overlay");
+
+setTimeout(function(){
+    loader.style.display = 'none';
+    overlay.style.display = 'none';
+},5000)
+
+
+
+
+
